366b9dc9fda55a9cb9b98f0830f4b46d99fa56ca
Your Name <you@example.com>
2022-12-21 12:07:05 +0000

    logs

--- function/sendToMega.js
+++ function/sendToMega.js
@@ -5,7 +5,6 @@ const sendToMega = async (req, res) => {
   try {
     console.log("called");
     const { data } = req.body;
-
     fs.writeFile("hello.txt", data, () => {
       console.log("file created");
     });
@@ -29,7 +28,7 @@ const sendToMega = async (req, res) => {
     spawnedShell.stdin.end();
     res.status(200).json({ message: "success" });
   } catch (error) {
-    console.log(error);
+    console.log("error on sendToMega", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
