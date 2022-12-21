e7b90427d6f69303c225bb74a76b9ce45455f31d
Your Name <you@example.com>
2022-12-21 11:18:24 +0000

    sendToMega function added

---
+++ function/sendToMega.js
@@ -0,0 +1,38 @@
+const { spawn } = require("child_process");
+const fs = require("fs");
+
+const sendToMega = async (req, res) => {
+  try {
+    console.log("called");
+    const { data } = req.body;
+    // const data = "hello world 123";
+
+    fs.writeFile("hello.txt", data, () => {
+      console.log("file created");
+    });
+    const spawnedShell = spawn("mega-cmd");
+    spawnedShell.stdin.write(
+      `login ${process.env.MEGA_UPLOAD_LOGIN_URL} ${process.env.MEGA_UPLOAD_PASSWORD} \n`
+    );
+    spawnedShell.stdin.write("put ../hello.txt \n");
+
+    spawnedShell.stdout.on("data", (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    spawnedShell.stderr.on("data", (data) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    spawnedShell.on("close", (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+    spawnedShell.stdin.end();
+    res.status(200).json({ message: "success" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+module.exports = sendToMega;
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const app = require('./app')
+require("dotenv").config();
 
-const port = process.env.PORT || 3000
+const app = require("express")();
+const port = process.env.PORT || 4000;
+
+app.use("/get-file", require("./function/sendToMega"));
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+  // require("./function/sendToMega")();
+});
