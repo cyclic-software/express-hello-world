4761223e6f867d56485ac2bda30135b48de72516
Your Name <you@example.com>
2022-12-24 10:59:24 +0000

    file upload client side

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const upload = require("./function/fileUpload");
 
 const app = express();
 app.use(cors());
@@ -8,7 +9,10 @@ app.use(express.json({ extended: false, limit: "250mb" }));
 
 const port = process.env.PORT || 5000;
 
-app.use("*", require("./function/sendToMega"));
+app.use("file-upload", upload.single("file"), require("./function/sendToMega"));
+app.use("*", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
