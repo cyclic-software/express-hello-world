876052ad75efb99c610fe28624c4b1b152a3a679
Your Name <you@example.com>
2022-12-24 11:12:40 +0000

    rote fix

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ app.use(express.json({ extended: false, limit: "250mb" }));
 
 const port = process.env.PORT || 5000;
 
-app.use("file-upload", upload.single("file"), require("./function/sendToMega"));
+app.use(
+  "/file-upload",
+  upload.single("file"),
+  require("./function/sendToMega")
+);
 
 app.use(express.static(path.join(__dirname, "/public")));
 app.use("*", (req, res) => {
