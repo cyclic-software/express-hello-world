c6adfd8c6e327e58c836e02898aa141749128222
Your Name <you@example.com>
2022-12-26 08:24:14 +0000

    urlencoded and json added

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const multer = require("multer");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "25mb", extended: true }));
+app.use(
+  express.urlencoded({ limit: "250mb", extended: true, parameterLimit: 50000 })
+);
 
 const port = process.env.PORT || 5000;
 
