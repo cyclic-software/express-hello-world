844e2471361812bb82980ddfe97ad44353c0b0ae
Your Name <you@example.com>
2022-12-26 09:59:23 +0000

    body-parser added

--- server.js
+++ server.js
@@ -9,7 +9,16 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: 200000000, extended: false }));
+app.use(express.json({ limit: "500mb" }));
+app.use(bodyParser.json({ limit: "500mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "500mb",
+    extended: true,
+    parameterLimit: 100000000,
+  })
+);
+
 // app.use(
 //   express.urlencoded({
 //     limit: "250mb",
