30be0f99ce9f144e8728849e1e4a4dac097a013e
Your Name <you@example.com>
2022-12-26 07:08:38 +0000

    limit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const fs = require("fs");
 
 const app = express();
 app.use(cors());
-app.use(express.json({ extended: false, limit: "250mb" }));
+app.use(express.json({ limit: "250mb" }));
 
 const port = process.env.PORT || 5000;
 
