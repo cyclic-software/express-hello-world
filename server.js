7a66e67b761824e5c27b4575dc47bcd59a4ce98d
Your Name <you@example.com>
2022-12-26 08:59:03 +0000

    extended: false

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const fileUpload = require("./function/fileUpload");
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: "500mb", extended: true }));
-app.use(
-  express.urlencoded({
-    limit: "250mb",
-    extended: true,
-    parameterLimit: 5000000,
-  })
-);
+app.use(express.json({ limit: "500mb", extended: false }));
+// app.use(
+//   express.urlencoded({
+//     limit: "250mb",
+//     extended: true,
+//     parameterLimit: 5000000,
+//   })
+// );
 
 const port = process.env.PORT || 5000;
 
