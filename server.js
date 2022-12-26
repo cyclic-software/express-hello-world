36b98cb88e0d55a384ff2a962b7c16e02bde15ca
Your Name <you@example.com>
2022-12-26 08:28:32 +0000

    parameter limit increacing

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "25mb", extended: true }));
 app.use(
-  express.urlencoded({ limit: "250mb", extended: true, parameterLimit: 50000 })
+  express.urlencoded({
+    limit: "250mb",
+    extended: true,
+    parameterLimit: 5000000,
+  })
 );
 
 const port = process.env.PORT || 5000;
