0943bfe4eac4dc10c76af68621089c1f47fbede7
William Nash <nashswilliam@gmail.com>
2023-01-01 21:05:14 -0600

    testing custom routes

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(function (req, res, next) {
 //   folder: '/public'
 // }
 // app.use(express.static('public', options))
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('/public'));
 
 // #############################################################################
 app.get('/home', function (req, res) {
