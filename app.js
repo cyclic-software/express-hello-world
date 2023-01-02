c7f98bc696b6a314911b9dd2d7ec9ccf7077f0e6
William Nash <nashswilliam@gmail.com>
2023-01-02 08:26:39 -0600

    trying new db routing style

--- app.js
+++ app.js
@@ -1,14 +1,49 @@
 const express = require('express')
 const path = require("path");
+const sqlite = require("better-sqlite3");
 const app = express()
 
-// #############################################################################
+
+// ################################ DateBase #######################################
+/* fetch_folder_DB
+	Fetches the folder DB and returns the rows
+
+	- path: path to the DB
+	- returns: rows from the DB
+*/
+function fetch_folder_DB(path) {
+	var db = new sqlite(path);
+	var rows = db.prepare('SELECT * FROM folders').all();
+	// console.log(rows[0]);
+	db.close();
+	return rows;
+
+}
+
+/* process_DB
+	Processes the rows from the DB and returns a JSON object
+
+	- rows: rows from the DB
+	- returns: JSON object
+*/
+function process_DB(rows) {
+	let data = {};
+	rows.forEach((row) => {
+		data[row.folder_name] = {
+			display_name: row.display_name,
+			description: row.description,
+			imgur_album_id: row.imgur_album_id
+		};
+	});
+	return data;
+}
+// ################################ App Setup #######################################
 // Logs all request paths and method
 app.use(function (req, res, next) {
-  res.set('x-timestamp', Date.now())
-  res.set('x-powered-by', 'cyclic.sh')
-  console.log(`[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.path}`);
-  next();
+	res.set('x-timestamp', Date.now())
+	res.set('x-powered-by', 'cyclic.sh')
+	console.log(`[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.path}`);
+	next();
 });
 
 
@@ -16,47 +51,96 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname);
 
-// #############################################################################
 // This configures static hosting for files in /public that have the extensions
 // listed in the array.
 var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html', 'css', 'js', 'ico', 'jpg', 'jpeg', 'png', 'svg'],
-  index: ['index.html'],
-  maxAge: '1m',
-  redirect: false,
-  folder: '/public'
+	dotfiles: 'ignore',
+	etag: false,
+	extensions: ['htm', 'html', 'css', 'js', 'ico', 'jpg', 'jpeg', 'png', 'svg'],
+	index: ['index.html'],
+	maxAge: '1m',
+	redirect: false,
+	folder: '/public'
 }
 app.use(express.static('public', options))
 // app.use(express.static('public'));
 
-// #############################################################################
+
+// ############################ Routing Helpers ####################################
+/* getImageAlbum
+	- Gets all images in a imgur album by id
+
+	- param: albumId - the id of the album
+	- return: image_list - the list of images in the album
+*/
+function getImageAlbum(albumId) {
+	const options = {
+		url: `https://api.imgur.com/3/album/${albumId}`,
+		headers: {
+			'Authorization': `Client-ID ${clientId}`
+		}
+	};
+	let image_list = [];
+	return new Promise((resolve, reject) => {
+		request(options, (error, response, body) => {
+			if (error) {
+				console.error(error);
+			} else {
+				try {
+					const data = JSON.parse(body);
+					for (const image of data.data.images) {
+						image_list.push(image.link);
+					}
+					resolve(image_list);
+				} catch (e) {
+					// console.error(e);
+					console.error("Data: " + body);
+				}
+			}
+		});
+
+	});
+}
+// ################################ Routing ########################################
+let folder_data = process_DB(fetch_folder_DB(__dirname + '/public/db.sqlite'));
+
 app.get('/home', function (req, res) {
-  res.render(__dirname + '/public/home.html')
+	res.render(__dirname + '/public/home.html')
 })
 
+/* Index page
+	- Waits for data from db to be loaded in, then renders the index.html
+*/
 app.get('/folders', function (req, res) {
-  res.render(__dirname + '/public/folders.html', {
-    folders: ['folder1', 'folder2', 'folder3']
-  })
+	res.render(__dirname + '/public/folders.html', {
+		folders: Object.keys(folder_data)
+	})
 })
 
-// #############################################################################
+/* Folder page
+	- Waits for data from db to be loaded in, then gets the image list from imgur
+*/
+for (const folder of Object.keys(data)) {
+	const imgPath = '/' + folder;
+	getImageAlbum(data[folder]['imgur_album_id'])
+		.then((image_list) => {
+			app.get(imgPath, function (req, res) {
+				var title = data[folder]['display_name'];
+				var description = data[folder]['description'];
+				res.render(__dirname + '/public/html/template_grid.html', {
+					image_links: image_list,
+					title: title,
+					description: description
+				});
+			})
+
+		});
+}
+
+// ################################ Error Handling #################################
 // Catch all handler for all other request.
 app.use('*', (req, res) => {
-  // res.json({
-  //   at: new Date().toISOString(),
-  //   method: req.method,
-  //   hostname: req.hostname,
-  //   ip: req.ip,
-  //   query: req.query,
-  //   headers: req.headers,
-  //   cookies: req.cookies,
-  //   params: req.params
-  // })
-  //   .end()
-  res.status(404).send('<h1>404! Page not found</h1>');
+	res.status(404).send('<h1>404! Page not found</h1>');
 })
 
 module.exports = app
