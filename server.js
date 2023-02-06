11f84cd6614bfbe2d69e9fc5449e367fb7fe358d
Ruth Reed <ruthreed.dev@gmail.com>
2023-02-06 00:22:39 -0700

    :lipstick: (styling) update styling and add span instructions

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
