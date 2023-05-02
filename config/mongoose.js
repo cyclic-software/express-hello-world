bc31a78403f797db3fb9fa4263ba3bbed6390660
Sushmit Katkale <72334943+SushmitKatkale@users.noreply.github.com>
2023-05-02 09:23:27 +0530

    main

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(''mongodb+srv://katkalesushmit:NYtKNTfGHvFB1Ulw@cluster0.89lyzv0.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://katkalesushmit:NYtKNTfGHvFB1Ulw@cluster0.89lyzv0.mongodb.net/?retryWrites=true&w=majority');
 
 const db = mongoose.connection;
 
