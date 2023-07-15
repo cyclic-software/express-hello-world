55e5bbe0fb03e56c41bcf069c4f863c3062ac680
Muhammadziyo Valiyev <109217302+Muhammadziyo-web@users.noreply.github.com>
2023-07-15 18:56:56 +0500

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ app.use(function (req, res, next) {
   console.log(`[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.path}`);
   next();
 });
-
 // #############################################################################
 // This configures static hosting for files in /public that have the extensions
 // listed in the array.
