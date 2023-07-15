c1ebed87f2beba4546b1400979459ca5e84c0cc3
Muhammadziyo Valiyev <109217302+Muhammadziyo-web@users.noreply.github.com>
2023-07-15 18:56:45 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const path = require("path");
 const app = express()
-
 // #############################################################################
 // Logs all request paths and method
 app.use(function (req, res, next) {
