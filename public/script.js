c26872f53c340ee0f976890588e5cbf1bd02e924
jemonteni <jemonteni@yandex.ru>
2023-11-03 18:42:10 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,47 +1,14 @@
-const chatForm = document.getElementById('chatForm');
-const userMessageInput = document.getElementById('userMessage');
-const chatLog = document.getElementById('chatLog');
-
-chatForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  const userMessage = userMessageInput.value;
-
-  const requestBody = {
-    "model": "gpt-3.5-turbo",
-    "messages": [
-      {
-        "role": "user",
-        "content": userMessage
-      }
-    ],
-    "stream": false
-  };
-
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      'Authorization': 'Bearer sk-76ADBqR4Eqsto6K8p8DNVGhlQi5BSQBGIxu8MKv2E6mla1KY', // Замените $API_KEY на ваш ключ авторизации
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(requestBody)
-  };
-
-  fetch('https://api.theb.ai/v1/chat/completions', requestOptions)
-    .then(response => response.json())
-    .then(data => {
-      const assistantReply = data.choices[0].message.content;
-      displayMessage('assistant', assistantReply);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-
-  userMessageInput.value = '';
-});
-
-function displayMessage(role, content) {
-  const messageElement = document.createElement('p');
-  messageElement.innerHTML = `<strong>${role}: </strong>${content}`;
-  chatLog.appendChild(messageElement);
-}
+curl --location 'https://api.theb.ai/v1/chat/completions' \
+# curl --location 'https://api.baizhi.ai/v1/chat/completions' \
+--header 'Authorization: Bearer sk-76ADBqR4Eqsto6K8p8DNVGhlQi5BSQBGIxu8MKv2E6mla1KY' \
+--header 'Content-Type: application/json' \
+--data '{
+  "model": "gpt-3.5-turbo",
+  "messages": [
+    {
+      "role": "user",
+      "content": "Hello, World!"
+    }
+  ],
+  "stream": true
+}'
