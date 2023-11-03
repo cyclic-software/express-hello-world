0ab0867e750e72a543ebac07c82ec7e199b7b61c
jemonteni <jemonteni@yandex.ru>
2023-11-03 12:34:12 +0300

    Update chatGPT.js

--- public/js/chatGPT.js
+++ public/js/chatGPT.js
@@ -11,7 +11,7 @@ async function sendMessageToChatGPT(message) {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-         'Authorization': 'ApiKEY sk-76ADBqR4Eqsto6K8p8DNVGhlQi5BSQBGIxu8MKv2E6mla1KY'
+         'Authorization': 'Bearer sk-76ADBqR4Eqsto6K8p8DNVGhlQi5BSQBGIxu8MKv2E6mla1KY'
       },
       body: JSON.stringify({
         message: message
