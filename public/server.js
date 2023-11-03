ad531d4da2be9d7341048aa5c662f744e05a2b14
jemonteni <jemonteni@yandex.ru>
2023-11-03 13:48:36 +0300

    Update server.js

--- public/server.js
+++ public/server.js
@@ -1,1 +1,7 @@
+const httpServer = require('http-server');
+const server = httpServer.createServer();
+const port = 3000; // Укажите порт по вашему усмотрению
 
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
