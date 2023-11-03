f14b1d8fc1a350a08ac6dbb285e4dcd8bf83e822
jemonteni <jemonteni@yandex.ru>
2023-11-03 13:30:47 +0300

    Update get_response.js

--- public/js/get_response.js
+++ public/js/get_response.js
@@ -1,27 +1,35 @@
-const axios = require('axios');
+import axios from 'axios';
+import { OpenAI } from 'openai';
 
-let config = {
+const openai = new OpenAI({
+  apiKey: process.env.TheB_AI_API_KEY,
+  baseURL: 'https://api.theb.ai/v1'
+});
+
+const data = JSON.stringify({
+  "model": "claude-2",
+  "messages": [
+    {
+      "role": "user",
+      "content": "How are you?"
+    }
+  ],
+  "stream": false,
+  "model_params": {
+    "temperature": 1
+  }
+});
+
+const config = {
   method: 'post',
   maxBodyLength: Infinity,
-  url: 'https://api.theb.ai/v1/chat/completions',
-  // url: 'https://api.baizhi.ai/v1/chat/completions',
+  baseURL: 'https://api.theb.ai/v1',
+  url: '/chat/completions',
   headers: {
     'Authorization': 'Bearer sk-76ADBqR4Eqsto6K8p8DNVGhlQi5BSQBGIxu8MKv2E6mla1KY',
     'Content-Type': 'application/json'
   },
-  data: JSON.stringify({
-    "model": "claude-2",
-    "messages": [
-      {
-        "role": "user",
-        "content": "How are you?"
-      }
-    ],
-    "stream": false,
-    "model_params": {
-      "temperature": 1
-    }
-  })
+  data: data
 };
 
 axios.request(config)
@@ -31,3 +39,20 @@ axios.request(config)
   .catch((error) => {
     console.log(error);
   });
+
+async function fetchChatCompletion() {
+  const result = await openai.chat.completions.create({
+    model: 'claude-instant-1',
+    stream: false,
+    model_params: {
+      temperature: 0.8
+    },
+    messages: [{ 
+      role: 'user', 
+      content: 'Say hello!' 
+    }],
+  });
+  console.log(result);
+}
+
+fetchChatCompletion();
