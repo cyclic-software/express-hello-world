e5e8c43c4831d38345b79c1d48126d66bd8e9ac8
jemonteni <jemonteni@yandex.ru>
2023-11-03 18:29:12 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,19 +1,14 @@
-const axios = require('axios');
-
-let config = {
-  method: 'get',
-  maxBodyLength: Infinity,
-  url: 'https://api.theb.ai/v1/models',
-  // url: 'https://api.baizhi.ai/v1/models',
-  headers: {
-    'Authorization': 'Bearer sk-76ADBqR4Eqsto6K8p8DNVGhlQi5BSQBGIxu8MKv2E6mla1KY' // Замените $API_KEY на ваш ключ авторизации
-  }
-};
-
-axios.request(config)
-.then((response) => {
-  console.log(JSON.stringify(response.data));
-})
-.catch((error) => {
-  console.log(error);
-});
+curl --location 'https://api.theb.ai/v1/chat/completions' \
+# curl --location 'https://api.baizhi.ai/v1/chat/completions' \
+--header 'Authorization: Bearer sk-76ADBqR4Eqsto6K8p8DNVGhlQi5BSQBGIxu8MKv2E6mla1KY' \
+--header 'Content-Type: application/json' \
+--data '{
+  "model": "gpt-3.5-turbo",
+  "messages": [
+    {
+      "role": "user",
+      "content": "Hello, World!"
+    }
+  ],
+  "stream": false
+}'
