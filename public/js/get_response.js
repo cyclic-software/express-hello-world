4e392816818c599b0f2328d0dd1d588eaee418b3
jemonteni <jemonteni@yandex.ru>
2023-11-03 13:20:11 +0300

    Update get_response.js

--- public/js/get_response.js
+++ public/js/get_response.js
@@ -33,3 +33,28 @@ axios.request(config)
 .catch((error) => {
   console.log(error);
 });
+
+const { OpenAI } = require("openai");
+
+const openai = new OpenAI({
+  apiKey: process.env.TheB_AI_API_KEY,
+  baseURL: 'https://api.theb.ai/v1'
+  // baseURL: 'https://api.baizhi.ai/v1'
+});
+
+async function fetchChatCompletion() {
+  const result = await openai.chat.completions.create({
+    model: 'claude-instant-1',
+    stream: false,
+    model_params: {
+      temperature: 0.8
+    },
+    messages: [{ 
+      role: 'user', 
+      content: 'Say hello!' 
+    }],
+  });
+  console.log(result);
+}
+
+fetchChatCompletion();
