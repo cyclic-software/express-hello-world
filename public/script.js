7354f8b1d29048b4966287183c6c4398ee68fc35
jemonteni <jemonteni@yandex.ru>
2023-11-03 14:03:46 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,47 @@
+const chatForm = document.getElementById('chatForm');
+const userMessageInput = document.getElementById('userMessage');
+const chatLog = document.getElementById('chatLog');
 
+chatForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const userMessage = userMessageInput.value;
+  
+  const requestBody = {
+    "model": "gpt-3.5-turbo",
+    "messages": [
+      {
+        "role": "user",
+        "content": userMessage
+      }
+    ],
+    "stream": false
+  };
+  
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer $API_KEY', // Замените $API_KEY на ваш ключ авторизации
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(requestBody)
+  };
+  
+  fetch('https://api.theb.ai/v1/chat/completions', requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      const assistantReply = data.choices[0].message.content;
+      displayMessage('assistant', assistantReply);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  
+  userMessageInput.value = '';
+});
+
+function displayMessage(role, content) {
+  const messageElement = document.createElement('p');
+  messageElement.innerHTML = `<strong>${role}: </strong>${content}`;
+  chatLog.appendChild(messageElement);
+}
