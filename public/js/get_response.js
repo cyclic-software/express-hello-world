a302b77dcc42e18237ee46a394dd27402c963426
jemonteni <jemonteni@yandex.ru>
2023-11-03 13:40:49 +0300

    Update get_response.js

--- public/js/get_response.js
+++ public/js/get_response.js
@@ -56,3 +56,52 @@ async function fetchChatCompletion() {
 }
 
 fetchChatCompletion();
+
+// Отправка запроса к API
+const response = await axios.post('https://api.example.com/chat/completions', payload, headers);
+
+// Пример обработки ответа
+const completion = response.data.choices[0]; // Получение первого завершения чата
+const role = completion.message.role; // Получение роли в сообщении (assistant, user и т.д.)
+const content = completion.message.content; // Получение содержимого сообщения
+const finishReason = completion.finish_reason; // Получение причины завершения чата
+
+// Дальнейшая обработка ответа, например, вывод в консоль
+console.log(`Role: ${role}`);
+console.log(`Content: ${content}`);
+console.log(`Finish Reason: ${finishReason}`);
+
+const axios = require('axios');
+let data = JSON.stringify({
+  "model": "claude-2",
+  "messages": [
+    {
+      "role": "user",
+      "content": "How are you?"
+    }
+  ],
+  "stream": true,
+  "model_params": {
+    "temperature": 1
+  }
+});
+
+let config = {
+  method: 'post',
+  maxBodyLength: Infinity,
+  url: 'https://api.theb.ai/v1/chat/completions',
+  // url: 'https://api.baizhi.ai/v1/chat/completions',
+  headers: {
+    'Authorization': 'Bearer sk-76ADBqR4Eqsto6K8p8DNVGhlQi5BSQBGIxu8MKv2E6mla1KY',
+    'Content-Type': 'application/json'
+  },
+  data : data
+};
+
+axios.request(config)
+.then((response) => {
+  console.log(JSON.stringify(response.data));
+})
+.catch((error) => {
+  console.log(error);
+});
