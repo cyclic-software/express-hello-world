2fb3f91db448b3105256d420cee7ecc0a8fae390
jemonteni <jemonteni@yandex.ru>
2023-11-03 13:16:01 +0300

    Update get_response.js

--- public/js/get_response.js
+++ public/js/get_response.js
@@ -1,1 +1,35 @@
 
+const axios = require('axios');
+let data = JSON.stringify({
+  "model": "claude-2",
+  "messages": [
+    {
+      "role": "user",
+      "content": "How are you?"
+    }
+  ],
+  "stream": false,
+  "model_params": {
+    "temperature": 1
+  }
+});
+
+let config = {
+  method: 'post',
+  maxBodyLength: Infinity,
+  url: 'https://api.theb.ai/v1/chat/completions',
+  // url: 'https://api.baizhi.ai/v1/chat/completions',
+  headers: {
+    'Authorization': 'Bearer $API_KEY',
+    'Content-Type': 'application/json'
+  },
+  data : data
+};
+
+axios.request(config)
+.then((response) => {
+  console.log(JSON.stringify(response.data));
+})
+.catch((error) => {
+  console.log(error);
+});
