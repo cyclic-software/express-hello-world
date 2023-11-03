b0681740730c94e226b0e265948b42f6416ab25e
jemonteni <jemonteni@yandex.ru>
2023-11-03 13:20:52 +0300

    Update server.js

--- public/js/server.js
+++ public/js/server.js
@@ -1,41 +1,36 @@
 // server.js
-
 const express = require('express');
+const { OpenAI } = require("openai");
+
 const app = express();
 const port = 3000;
 
-// Разрешаем обслуживать статические файлы из папки "public"
-app.use(express.static('public'));
+app.get('/fetchChatCompletion', async (req, res) => {
+  const openai = new OpenAI({
+    apiKey: process.env.TheB_AI_API_KEY,
+    baseURL: 'https://api.theb.ai/v1'
+  });
 
-// Обработчик главной страницы
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  try {
+    const result = await openai.chat.completions.create({
+      model: 'claude-instant-1',
+      stream: false,
+      model_params: {
+        temperature: 0.8
+      },
+      messages: [{ 
+        role: 'user', 
+        content: 'Say hello!' 
+      }],
+    });
+    console.log(result);
+    res.send(result);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred while processing the request.' });
+  }
 });
 
-// Обработчик страницы административной панели
-app.get('/admin', (req, res) => {
-  res.sendFile(__dirname + '/admin.html');
-});
-
-// Обработчик страницы чата
-app.get('/chat', (req, res) => {
-  res.sendFile(__dirname + '/chat.html');
-});
-
-const data = await response.json();
-console.log('Response data:', data);
-
-const response = await axios.post('https://api.theb.ai/v1', {
-  model: 'gpt-3.5',
-  messages: [{ role: 'system', content: 'You are a helpful assistant.' }, { role: 'user', content: message }],
-}, {
-  headers: {
-    'Content-Type': 'application/json',
-     'Authorization': 'Bearer sk-76ADBqR4Eqsto6K8p8DNVGhlQi5BSQBGIxu8MKv2E6mla1KY'
-  },
-});
-
-// Запускаем сервер
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Server listening on port ${port}`);
 });
