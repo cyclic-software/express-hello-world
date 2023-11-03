925cc980f0bdf09fd7c2b483a0c2f07d982e5976
jemonteni <jemonteni@yandex.ru>
2023-11-03 13:12:52 +0300

    Update getModels.js

--- public/js/getModels.js
+++ public/js/getModels.js
@@ -2,7 +2,7 @@ import requests
 url = "https://api.theb.ai/v1/models"
 # url = "https://api.baizhi.ai/v1/models"
 headers = {
-  'Authorization': 'Bearer $API_KEY'
+  'Authorization': 'Bearer sk-76ADBqR4Eqsto6K8p8DNVGhlQi5BSQBGIxu8MKv2E6mla1KY'
 }
 response = requests.request("GET", url, headers=headers)
 print(response.json())
