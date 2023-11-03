fbc28048ebeaa0e3f110c2e1e1a11e9d75a1cc00
jemonteni <jemonteni@yandex.ru>
2023-11-03 10:13:01 +0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,26 @@
 
+// Common JavaScript code for both index.html and chat.html
+
+function getChatGPTResponse(message) {
+  const url = 'http://seohunter.ru/chatgpt'; // Replace with your server URL
+  const apiKey = 'sk-LHH8z2N9GfY6oz2hwsRzVGhlQi5BSQk91va2dYXF8tLYsu6V'; // Replace with your API key
+
+  return new Promise((resolve, reject) => {
+    axios.get(url, {
+      params: {
+        message: message
+      },
+      headers: {
+        'Authorization': `Bearer ${apiKey}`
+      }
+    })
+    .then(response => {
+      resolve(response.data.reply);
+    })
+    .catch(error => {
+      reject(error);
+    });
+  });
+}
+
+// Other shared functions or code can be added here
