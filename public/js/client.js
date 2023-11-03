4663d4d4a137bd1700670a3e1b2a3c6d909bbf4f
jemonteni <jemonteni@yandex.ru>
2023-11-03 09:56:38 +0300

    Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -1,1 +1,37 @@
+// client.js
 
+// Функция для добавления сообщения в список сообщений чата
+function addMessageToChat(message) {
+  const chatMessages = document.querySelector('.chat-messages');
+  const messageElement = document.createElement('div');
+  messageElement.classList.add('message');
+  messageElement.innerText = message;
+  chatMessages.appendChild(messageElement);
+}
+
+// Функция для отправки сообщения на сервер
+function sendMessage(message) {
+  // Здесь можно добавить логику для отправки сообщения на сервер
+  // Пример: просто добавляем сообщение в список сообщений чата
+  addMessageToChat('Вы: ' + message);
+}
+
+// Обработчик отправки формы
+function handleSubmit(event) {
+  event.preventDefault(); // Предотвращаем отправку формы
+
+  const messageInput = document.getElementById('message-input');
+  const message = messageInput.value.trim();
+
+  if (message !== '') {
+    // Отправляем сообщение
+    sendMessage(message);
+
+    // Очищаем поле ввода
+    messageInput.value = '';
+  }
+}
+
+// Привязываем обработчик отправки формы к форме чата
+const chatForm = document.getElementById('chat-form');
+chatForm.addEventListener('submit', handleSubmit);
